3aef4026443ba5b12134a13a6b4400964dd7d433
Ana Laura Andrade <analaura140600@gmail.com>
2024-09-15 20:42:39 -0300

    Add project files.

---
+++ questao5/Controllers/ProdutoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProdutoController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CriarProduto([FromBody] Produto produto)
+    {
+        if (!ModelState.IsValid)
+        {
+            var erros = ModelState.Values
+                                   .SelectMany(v => v.Errors)
+                                   .Select(e => e.ErrorMessage)
+                                   .ToList();
+            return BadRequest(erros);
+        }
+
+        return CreatedAtAction(nameof(CriarProduto), new { id = 1 }, produto); 
+    }
+
+}
---
+++ questao5/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Produto
+{
+    [Required(ErrorMessage = "Descrição obrigatória!")]
+    public string Descricao { get; set; }
+
+    public double Preco { get; set; }
+
+    public int Estoque { get; set; }
+
+    [Required(ErrorMessage = "Código do produto não pode estar vazio")]
+    [RegularExpression(@"^[A-Z]{3}-\d{4}$", ErrorMessage = "Código do produto deve seguir o formato AAA-1234")]
+    public string CodigoProduto { get; set; }
+}
